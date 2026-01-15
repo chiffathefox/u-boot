4f2a888caad6d8e36dc565b47f21716f6a235ea5
chiffathefox <chiffathefoxx@gmail.com>
2026-01-15 05:22:45 +0200

    board: beyond1lte: Samsung S10 board matching

--- board/samsung/exynos-mobile/exynos-mobile.c
+++ board/samsung/exynos-mobile/exynos-mobile.c
@@ -177,6 +177,14 @@ static struct exynos_board_info exynos_board_info_match[] = {
 		.match = exynos7870_fdt_match,
 		.match_model = "G610",
 		.match_max_rev = U8_MAX,
+	}, {
+		/* Samsung Galaxy S10 */
+		.name = "beyond1lte",
+		.chip = "exynos9820",
+		.dram_bank_bases = exynos7870_common_dram_bank_bases,
+		.match = exynos7870_fdt_match,
+		.match_model = "G973",
+		.match_max_rev = U8_MAX,
 	},
 };
 
