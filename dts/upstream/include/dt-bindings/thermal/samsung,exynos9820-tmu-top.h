a7a5f2a009c7f32ff894619d30f7d3cb7a7a658e
chiffathefox <chiffathefoxx@gmail.com>
2026-07-15 19:40:05 +0300

    wip: dts; tmu

---
+++ dts/upstream/include/dt-bindings/thermal/samsung,exynos9820-tmu-top.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (c) 2026 chiffathefox <chiffathefoxx@gmail.com>
+ *
+ * Device Tree binding constants for Samsung exynos9820 TMU clock controller.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_SAMSUNG_EXYNOS9820_TMU_TOP_H
+#define _DT_BINDINGS_CLOCK_SAMSUNG_EXYNOS9820_TMU_TOP_H
+
+#define EXYNOS9820_TMU_TOP_CPUCL2			0
+#define EXYNOS9820_TMU_TOP_CPUCL1			1
+#define EXYNOS9820_TMU_TOP_CPUCL0			2
+#define EXYNOS9820_TMU_TOP_G3D  			3
+#define EXYNOS9820_TMU_TOP_ISP  			4
+
+#endif /* _DT_BINDINGS_CLOCK_SAMSUNG_EXYNOS9820_TMU_TOP_H */
